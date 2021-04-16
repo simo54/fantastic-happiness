173ea3f2fb8fc1c5d2a3b899348e7fd2d7b885c0
simone <sorzesimone1@gmail.com>
2021-04-16 21:11:35 +0200

    implementing routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
+    <div style={{ display: 'flex', height: '100vh', alignItems: 'stretch' }}>
+      <div style={{ flex: 0.3, backgroundColor: '#f2f2f2' }}>route menu</div>
+      <div>content</div>
     </div>
   );
 }
