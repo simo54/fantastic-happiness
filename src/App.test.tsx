5941a6d4e489bf0e7be6a5d4f48f8924f3ee105b
simone <sorzesimone1@gmail.com>
2021-04-17 16:34:43 +0200

    should not commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,6 +3,10 @@ import App from './App'
 
 test('renders learn react link', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
+  const linkElement = screen.getByText(/route menu/i)
   expect(linkElement).toBeInTheDocument()
+})
+
+it('Exists', () => {
+  expect(2 + 3).toBe(4)
 })
